db93522a28852369428441582ce75129f89992c0
Vishesh Mangla <manglavishesh64@gmail.com>
2023-03-28 00:53:55 +0530

    chore: removed task 2 additional endpoint(old implementation)

--- index.js
+++ index.js
@@ -30,36 +30,6 @@ app.route("/api/v1/tree").get(async (req, res) => {
   }
 });
 
-app
-  .route("/api/v1/tree/2")
-  .get((_, res) => {
-    res.status(100).json({
-      status: "success",
-      message: `Send a POST request to this url in 'x-www-form-urlencoded' format`,
-    });
-  })
-  .post(async (req, res) => {
-    const text = req.body.text;
-
-    try {
-      const filePath = __dirname + "/data.txt";
-      await fs.promises.writeFile(filePath, text, "utf8");
-
-      let deserializer = new DeserializerText(filePath);
-      await deserializer.deserialize();
-
-      res.status(200).json({
-        status: "success",
-        data: [deserializer.getJsonTree()],
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        status: "error",
-        message: `Error :\n${err.message}`,
-      });
-    }
-  });
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
