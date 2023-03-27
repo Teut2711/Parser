ee3dcb07ce9aff515f518f4f897549d77c782718
Vishesh Mangla <manglavishesh64@gmail.com>
2023-03-28 00:52:17 +0530

    fix: changed get method to return the file parsed content

--- Deserializer.js
+++ Deserializer.js
@@ -4,41 +4,44 @@ const Node = require("./Node");
 
 class DeserializerText {
   constructor(path) {
-    this._path = path;
+    this.#path = path;
   }
 
   async deserialize() {
-    let path = this._path;
+    let path = this.#path;
     try {
       let data = await fs.promises.readFile(path, "utf8");
       data = data.split("\n");
-      const [index, label] = this._getIndexAndLabel(data[0]);
+      const [index, label] = this.#getIndexAndLabel(data[0]);
       const root = new Node(index, label);
       let start = 1;
       let end = data.length - 1;
-      this._buildTree(root, data, start, end, 0);
-      this._root = root;
+      this.#buildTree(root, data, start, end, 0);
+      this.#root = root;
     } catch (err) {
       throw new Error(`Problem in building tree ${err.message}`);
     }
   }
-  getJsonTree() {
-    return this._root.getJson();
+  getTreeDS() {
+    return this.#root;
   }
-  _buildTree(root, data, start, end, parentIndent) {
+  getJsonTree() {
+    return this.#root.getJson();
+  }
+  #buildTree(root, data, start, end, parentIndent) {
     for (let i = start; i <= end; i++) {
       const currIndent = (data[i].match(/^\s*/) || [""])[0].length / 4;
       if (currIndent < parentIndent) {
         return;
       }
       if (currIndent === parentIndent + 1) {
-        const [index, label] = this._getIndexAndLabel(data[i]);
+        const [index, label] = this.#getIndexAndLabel(data[i]);
         root.addChildren(new Node(index, label));
       } else {
         let start = i;
         let end = data.length - 1;
 
-        this._buildTree(
+        this.#buildTree(
           root.children[root.children.length - 1],
           data,
           start,
@@ -50,7 +53,7 @@ class DeserializerText {
     }
   }
 
-  _getIndexAndLabel(str) {
+  #getIndexAndLabel(str) {
     const regexPattern = /^\s*\d+:\s*(.*)$/;
     const match = str.match(regexPattern);
 
--- index.js
+++ index.js
@@ -9,10 +9,34 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.route("/api/v1/tree").get(async (req, res) => {
+  try {
+    const filePath = __dirname + "/data.txt";
+    const text = await fs.promises.readFile(filePath);
+
+    let deserializer = new DeserializerText(filePath);
+    await deserializer.deserialize();
+
+    res.status(200).json({
+      status: "success",
+      data: [deserializer.getJsonTree()],
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "error",
+      message: `Error :\n${err.message}`,
+    });
+  }
+});
+
 app
-  .route("/api/v1/tree/1")
+  .route("/api/v1/tree/2")
   .get((_, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.status(100).json({
+      status: "success",
+      message: `Send a POST request to this url in 'x-www-form-urlencoded' format`,
+    });
   })
   .post(async (req, res) => {
     const text = req.body.text;
@@ -36,7 +60,6 @@ app
       });
     }
   });
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
